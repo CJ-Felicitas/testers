27e45b74e83cad5588e29fd698679fd1cb0cac03
CJ Felicitas <cjbfelicitas@usep.edu.ph>
2021-09-18 20:57:00 +0800

    update

--- generic_tester.java
+++ generic_tester.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 
 public class generic_tester {
 public static void main(String[] args) {
     generic_class<Integer> tester = new generic_class<Integer>(56);
-
+   
 
 tester.showType();
 
